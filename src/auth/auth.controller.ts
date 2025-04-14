253d7344aa2c2c2c792cd9bc29885198bc438d30
KPorus <designerfardin2021@gmail.com>
2025-04-14 20:47:59 +0600

    modify the login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ILoginBody, RegisterBodyDto } from './dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
-  login(@Body() dto: ILoginBody) {
-    return this.authService.login(dto);
+  login(@Body() dto: ILoginBody, @Res() res: Response) {
+    return this.authService.login(dto, res);
   }
   @Post('/register')
   register(@Body() dto: RegisterBodyDto) {
